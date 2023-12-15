bc51e0a700f75b7238ba6ef66daeae8cf7566fcf
wafaequo <w.quorsane@student.tue.nl>
2023-12-15 18:37:12 +0100

    everything_except_training

---
+++ full_code/Elements.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 15 15:41:57 2023
+
+@author: 20192032
+"""
+
+elements = ["He", "Li", "Be", "Ne",
+    "Na", "Mg", "Al", "Si", "Cl", "Ar", "Ca",
+    "Sc", "Ti", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn",
+    "Ga", "Ge", "As", "Se", "Br", "Kr", "Rb", "Sr", "Zr",
+    "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn",
+    "Sb", "Te", "Xe", "Cs", "Ba", "La", "Ce", "Pr", "Nd",
+    "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb",
+    "Lu", "Hf", "Ta", "Re", "Os", "Ir", "Pt", "Au", "Hg",
+    "Tl", "Pb", "Bi", "Po", "At", "Rn", "Fr", "Ra", "Ac", "Th",
+    "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm",
+    "Md", "No", "Lr"
+]
+
+lc_elements = ["H", "B", "C", "N", "O", "F", "P", "S", "K", "V", "Y", "I", "W", "U", "D", "L", "R","h", "b", "c", "n", "o", "f", "p", "s", "k", "v", "y", "i", "w", "u", "d", "l", "r", "-", "+", "(", ")", "[", "]", "#", "=", "%",'0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
