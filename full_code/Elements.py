e7600b14e2894eb2963d393cb10df910ff19034a
wafaequo <w.quorsane@student.tue.nl>
2023-12-18 17:25:27 +0100

    One_hot_adapted

--- full_code/Elements.py
+++ full_code/Elements.py
@@ -15,7 +15,9 @@ elements = ["He", "Li", "Be", "Ne",
     "Lu", "Hf", "Ta", "Re", "Os", "Ir", "Pt", "Au", "Hg",
     "Tl", "Pb", "Bi", "Po", "At", "Rn", "Fr", "Ra", "Ac", "Th",
     "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm",
-    "Md", "No", "Lr"
-]
+    "Md", "No", "Lr", "he", "li", "be", "ne", "na", "mg", "al", "si", "cl", "ar", "ca", "sc", "ti", "cr", "mn", "fe", "co", "ni", "cu", "zn", "ga", "ge", "as", "se", "br", "kr", "rb", "sr", "zr", "nb", "mo", "tc", "ru", "rh", "pd", "ag", "cd", "in", "sn", "sb", "te", "xe", "cs", "ba", "la", "ce", "pr", "nd", "pm", "sm", "eu", "gd", "tb", "dy", "ho", "er", "tm", "yb", "lu", "hf", "ta", "re", "os", "ir", "pt", "au", "hg", "tl", "pb", "bi", "po", "at", "rn", "fr", "ra", "ac", "th", "pa", "u", "np", "pu", "am", "cm", "bk", "cf", "es", "fm", "md", "no", "lr"]
 
-lc_elements = ["H", "B", "C", "N", "O", "F", "P", "S", "K", "V", "Y", "I", "W", "U", "D", "L", "R","h", "b", "c", "n", "o", "f", "p", "s", "k", "v", "y", "i", "w", "u", "d", "l", "r", "-", "+", "(", ")", "[", "]", "#", "=", "%",'0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+lc_elements = ['H', 'B', 'C', 'N', 'O', 'F', 'P', 'S', 'K', 'V', 'Y', 'I', 'W', 'U', 'h', 'b', 'c', 'n', 'o', 'f', 'p', 's', 'k', 'v', 'y', 'i', 'w', 'u', '-', '+', '(', ')', '[', ']', '#', '=', '%', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+
