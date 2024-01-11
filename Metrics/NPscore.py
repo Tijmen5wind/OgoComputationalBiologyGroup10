4599de2e1b46cd019ef4cde0d3056a34905ccf67
Ferrisharte <152646009+Ferrisharte@users.noreply.github.com>
2024-01-11 19:33:40 +0100

    Update NPscore.py

--- Metrics/NPscore.py
+++ Metrics/NPscore.py
@@ -9,13 +9,15 @@ from rdkit.Contrib.NP_Score import npscorer
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-def plot_np_scores_distribution_with_canonicalization(smiles_list, output_filename='subset_20000_NP'):
+def list_np_scores_distribution_with_canonicalization(smiles_list):
     """
-    Plot the distribution of Natural Product (NP) scores for a list of molecules after canonicalization.
+    List the distribution of Natural Product (NP) scores for a list of molecules after canonicalization.
 
     Parameters:
     - smiles_list (list): List of SMILES strings.
-    - output_filename (str): The filename for saving the plot. Default is 'subset_20000_NP'.
+
+    Output:
+    - np_scores (list): List of NP scores
     """
 
     np_scores = []
@@ -29,19 +31,11 @@ def plot_np_scores_distribution_with_canonicalization(smiles_list, output_filena
             # Calculate NP score using the npscorer function
             np_score, confidence = npscorer.scoreMolWConfidence(Chem.MolFromSmiles(canonical_smiles), fscore)
             np_scores.append(np_score)
-
-    # Plot the distribution using Seaborn
-    sns.kdeplot(np_scores, color='green', fill=True)
-    plt.title('Distribution of NP Scores')
-    plt.xlabel('NP Score')
-    plt.ylabel('Density')
-
-    # Save the plot as 'subset_20000_NP.jpg'
-    plt.savefig(f'{output_filename}.jpg', dpi=300)
-    plt.show()
+    return np_scores
 
 
-plot_np_scores_distribution_with_canonicalization(lijstje)
+
+
 
 
 
