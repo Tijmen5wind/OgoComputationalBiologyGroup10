02bdb08d9ab1649f5a7f9f55e30a17921cb44788
Ferrisharte <152646009+Ferrisharte@users.noreply.github.com>
2024-01-12 13:07:46 +0100

    Update SAscore.py

--- Metrics/SAscore.py
+++ Metrics/SAscore.py
@@ -10,13 +10,15 @@ from rdkit.Contrib.SA_Score import sascorer
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-def plot_sa_scores_distribution_with_canonicalization(smiles_list, output_filename):
+def list_sa_scores_distribution_with_canonicalization(smiles_list):
     """
     Plot the distribution of Synthetic Accessibility (SA) scores for a list of molecules after canonicalization.
 
     Parameters:
     - smiles_list (list): List of SMILES strings.
-    - output_filename (str): The filename for saving the plot. Default is 'subset_20000_SA'.
+
+    Output:
+    - sa_scores (list) : List of sa_scores
     """
 
     sa_scores = []
@@ -30,15 +32,7 @@ def plot_sa_scores_distribution_with_canonicalization(smiles_list, output_filena
             sa_score = sascorer.calculateScore(Chem.MolFromSmiles(canonical_smiles))
             sa_scores.append(sa_score)
 
-    # Plot the distribution using Seaborn
-    sns.kdeplot(sa_scores, color='blue', fill=True)
-    plt.title('Distribution of SA Scores')
-    plt.xlabel('SA Score')
-    plt.ylabel('Density')
-
-    # Save the plot as 'subset_20000_SA.jpg'
-    plt.savefig(f'{output_filename}.jpg', dpi=300)
-    plt.show()
+    return sa_scores
 
 
 
